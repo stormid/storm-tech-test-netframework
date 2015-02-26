a71c1612459fe601fe2c30186ce999e92ad9e610
Tom <tom.e.glenn@gmail.com>
2015-02-26 20:31:26 +0000

    Implemented pagination into the card browser

--- Storm.InterviewTest.Hearthstone/Models/CardsSearchViewModel.cs
+++ Storm.InterviewTest.Hearthstone/Models/CardsSearchViewModel.cs
@@ -8,5 +8,9 @@ namespace Storm.InterviewTest.Hearthstone.Models
         public IEnumerable<string> PlayerClasses { get; set; }
         public IEnumerable<CardModel> Cards { get; set; }
         public string SearchTerm { get; set; }
+        public string PlayerClass { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalCards { get; set; }
     }
 }
