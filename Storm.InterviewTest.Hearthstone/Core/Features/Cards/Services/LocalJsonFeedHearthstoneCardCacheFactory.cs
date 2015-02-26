dbf1c3cfe0fe8bb18bf3311d1e70d0ff91ca0bd3
Tom <tom.e.glenn@gmail.com>
2015-02-26 19:58:21 +0000

    Refactored the LocalJsonFeedHearthstoneCardCacheFactory class to remove code duplication. Also made _parser protected in HearthstoneCardCacheFactory and removed the need to accept a parser as a parameter of the PopulateCards method.

--- Storm.InterviewTest.Hearthstone/Core/Features/Cards/Services/LocalJsonFeedHearthstoneCardCacheFactory.cs
+++ Storm.InterviewTest.Hearthstone/Core/Features/Cards/Services/LocalJsonFeedHearthstoneCardCacheFactory.cs
@@ -8,61 +8,42 @@ namespace Storm.InterviewTest.Hearthstone.Core.Features.Cards.Services
 {
 	public class LocalJsonFeedHearthstoneCardCacheFactory : HearthstoneCardCacheFactory
 	{
+	    private JObject _cardSets;
+
 		public LocalJsonFeedHearthstoneCardCacheFactory(IHearthstoneCardParser parser) : base(parser)
 		{
 		}
 
-		protected override IEnumerable<ICard> PopulateCards(IHearthstoneCardParser parser)
+		protected override IEnumerable<ICard> PopulateCards()
 		{
-			using (var reader = File.OpenText(HttpContext.Current.Server.MapPath("~/App_Data/cards.json")))
-			{
-				var cardSets = JObject.Parse(reader.ReadToEnd());
-				JToken cards;
-				if (cardSets.TryGetValue("Basic", out cards) && cards.Type == JTokenType.Array)
-				{
-					foreach (var card in cards)
-					{
-						var parsedCard = parser.Parse(card.ToString());
-						if (parsedCard != null)
-						{
-							yield return parsedCard;
-						}
-					}
-				}
-				if (cardSets.TryGetValue("Classic", out cards) && cards.Type == JTokenType.Array)
-				{
-					foreach (var card in cards)
-					{
-						var parsedCard = parser.Parse(card.ToString());
-						if (parsedCard != null)
-						{
-							yield return parsedCard;
-						}
-					}
-				}
-				if (cardSets.TryGetValue("Curse of Naxxramas", out cards) && cards.Type == JTokenType.Array)
-				{
-					foreach (var card in cards)
-					{
-						var parsedCard = parser.Parse(card.ToString());
-						if (parsedCard != null)
-						{
-							yield return parsedCard;
-						}
-					}
-				}
-				if (cardSets.TryGetValue("Goblins vs Gnomes", out cards) && cards.Type == JTokenType.Array)
-				{
-					foreach (var card in cards)
-					{
-						var parsedCard = parser.Parse(card.ToString());
-						if (parsedCard != null)
-						{
-							yield return parsedCard;
-						}
-					}
-				}
-			}
+		    using (var reader = File.OpenText(HttpContext.Current.Server.MapPath("~/App_Data/cards.json")))
+		    {
+		        _cardSets = JObject.Parse(reader.ReadToEnd());
+		    }
+
+		    var cards = new List<ICard>();
+		    cards.AddRange(GetCardSet("Basic"));
+            cards.AddRange(GetCardSet("Classic"));
+		    cards.AddRange(GetCardSet("Curse of Naxxramas"));
+		    cards.AddRange(GetCardSet("Goblins vs Gnomes"));
+
+		    return cards;
 		}
+
+	    public IEnumerable<ICard> GetCardSet(string name)
+        {
+            JToken cards;
+            if (_cardSets.TryGetValue(name, out cards) && cards.Type == JTokenType.Array)
+            {
+                foreach (var card in cards)
+                {
+                    var parsedCard = _parser.Parse(card.ToString());
+                    if (parsedCard != null)
+                    {
+                        yield return parsedCard;
+                    }
+                }
+            }
+	    }
 	}
 }
